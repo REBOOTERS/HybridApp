f20d266046487e8295f91f502a3d4b900269ce0f
REBOOTERS <foreverlove.zyl@163.com>
2018-02-09 18:29:46 +0800

    update gradle

--- release-bundle.cmd
+++ release-bundle.cmd
@@ -3,4 +3,9 @@
 echo react-native bundle --platform android --dev false --entry-file index.js --bundle-output app/src/main/assets/index.bundle --assets-dest app/src/main/res/
 
 
-react-native bundle --platform android --dev false --entry-file index.js --bundle-output app/src/main/assets/index.bundle --assets-dest app/src/main/res/
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output app/src/main/assets/index.bundle --assets-dest app/src/main/res/ && echo gradlew assembleRelease && gradlew assembleRelease
+
+
+
+
+
