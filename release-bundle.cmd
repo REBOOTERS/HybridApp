9611511b108604a50226f7fd00dad06a7ff3068d
REBOOTERS <foreverlove.zyl@163.com>
2018-02-09 16:44:26 +0800

    add a relase.jks

--- release-bundle.cmd
+++ release-bundle.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-echo react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/com/your-company-name/app-package-name/src/main/assets/index.android.bundle --assets-dest android/com/your-company-name/app-package-name/src/main/res/
+echo react-native bundle --platform android --dev false --entry-file index.js --bundle-output app/src/main/assets/index.android.bundle --assets-dest app/src/main/res/
 
 
 react-native bundle --platform android --dev false --entry-file index.js --bundle-output app/src/main/assets/index.android.bundle --assets-dest app/src/main/res/
