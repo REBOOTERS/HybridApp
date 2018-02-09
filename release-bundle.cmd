4f07a0a2ef56eca220a9c8a931097cbeef7a2d8e
REBOOTERS <foreverlove.zyl@163.com>
2018-02-09 16:51:21 +0800

    relase able apk

--- release-bundle.cmd
+++ release-bundle.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-echo react-native bundle --platform android --dev false --entry-file index.js --bundle-output app/src/main/assets/index.android.bundle --assets-dest app/src/main/res/
+echo react-native bundle --platform android --dev false --entry-file index.js --bundle-output app/src/main/assets/index.bundle --assets-dest app/src/main/res/
 
 
-react-native bundle --platform android --dev false --entry-file index.js --bundle-output app/src/main/assets/index.android.bundle --assets-dest app/src/main/res/
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output app/src/main/assets/index.bundle --assets-dest app/src/main/res/
